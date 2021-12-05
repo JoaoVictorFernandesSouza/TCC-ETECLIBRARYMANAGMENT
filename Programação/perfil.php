f7abe1fafedb69cbd70685fa727bf7d8dc293f77
Joao Victor FS <joaovictorfernandessouza@gmail.com>
2021-12-05 20:36:14 -0300

    Atualiza link GitHub

--- Programação/perfil.php
+++ Programação/perfil.php
@@ -144,7 +144,7 @@
                   </svg>Site Etec</a>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                <a href="https://github.com/JoaoVictorFernandesSouza/TCC-ELM" style="font-weight: 600;"
+                <a href="https://github.com/JoaoVictorFernandesSouza/TCC-ETECLIBRARYMANAGMENT" style="font-weight: 600;"
                   class="mb-0 text-reset text-decoration-none" target="_blank">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
